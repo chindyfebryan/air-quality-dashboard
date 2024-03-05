d09b1a0f7f8061769e4a8c78986567892b318028
Chindy Febryan <45525155+chindyfebryan@users.noreply.github.com>
2024-03-05 18:24:56 +0800

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -5,8 +5,8 @@ import numpy as np
 # Title of the web app
 st.title('Random Number Generator')
 
-# Generating a random number
-random_number = np.random.randint(1, 101)
+st.subheader("Kesimpulan Pertanyaan 1:")
+st.write("Terlihat bahwa konsentrasi SO2 cenderung lebih tinggi pada awal tahun, terutama pada bulan Januari, Februari, dan Maret. Hal ini mungkin terkait dengan kondisi cuaca dan pola emisi dari berbagai sumber pada periode tersebut. Konsentrasi SO2 kemudian menurun secara bertahap sepanjang tahun, mencapai titik terendah pada bulan Agustus, sebelum kembali meningkat menuju akhir tahun. Sedangkan konsentrasi NO2 juga menunjukkan pola yang mirip dengan konsentrasi SO2. Konsentrasi NO2 mencapai puncak tertinggi pada bulan November dan Desember.")
 
-# Displaying the random number
-st.write('The random number is:', random_number)
+st.subheader("Kesimpulan Pertanyaan 2:")
+st.write("PM2.5 dan PM10: Terlihat bahwa konsentrasi PM2.5 dan PM10 cenderung tinggi pada malam hari dan pagi hari, yang mungkin terkait dengan aktivitas transportasi dan industri yang lebih rendah selama periode tersebut.\n\nSO2: Konsentrasi SO2 terlihat meningkat di pagi hari dan kemudian menurun sepanjang siang dan sore hari. Hal ini mungkin terkait dengan polusi udara dari industri dan transportasi yang aktif pada pagi hari.\n\nNO2: Konsentrasi NO2 cenderung tinggi di pagi hari, kemudian menurun menjelang sore hari. Ini juga bisa terkait dengan polusi dari kendaraan bermotor yang beroperasi pada waktu tersebut.\n\nO3: Konsentrasi O3, yang merupakan indikator kualitas udara yang lebih baik, cenderung meningkat di siang hari karena paparan sinar matahari dan reaksi kimia di atmosfer. Namun, terdapat perubahan yang cukup signifikan di malam hari.")
